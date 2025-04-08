6f57d90e0f4529cbde910123e132c119c249bd22
Ulugbekjon2007 <safarovulugbekjon@gmail.com>
2025-04-08 18:58:45 +0500

    created

--- WebFileSystem/WebFileSystem.Api/Controllers/StorageController.cs
+++ WebFileSystem/WebFileSystem.Api/Controllers/StorageController.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
+
+
+
 namespace WebFileSystem.Api.Controllers
 {
     [Route("api/storage")]
     [ApiController]
     public class StorageController : ControllerBase
     {
+       
 
-        [HttpGet("get")]
-        public string Rettt()
-        {
-            return "salom";
-        }
     }
 }
---
+++ WebFileSystem/WebFileSystem.StorageBroker/Services/IStorageBrokerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http; // Ensure this package is installed
+
+namespace WebFileSystem.StorageBroker.Services
+{
+    public interface IStorageBrokerService
+    {
+        Task SaveFileAsync(string filePath, IFormFile file);
+        Task CreateDirectoryAsync(string directoryPath);
+        Task<IEnumerable<string>> GetAllFilesAndDirectoriesAsync(string directoryPath);
+        Task DeleteFileAsync(string filePath);
+        Task DeleteDirectoryAsync(string directoryPath);
+        Task<string> DownloadFileAsync(string filePath);
+        Task<string> DownloadFolderAsZipAsync(string directoryPath);
+    }
+}
