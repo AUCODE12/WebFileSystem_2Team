78413b583e57b6808298fb612c56461a5c6ecc21
Your Name <you@example.com>
2025-04-09 13:18:58 +0500

    IStorageBrokerService and StorageBrokerService

---
+++ WebFileSystem/WebFileSystem.StorageBroker/Services/IStorageBrokerService.cs
@@ -0,0 +1,13 @@
+﻿namespace WebFileSystem.StorageBroker.Services
+{
+    public interface IStorageBrokerService
+    {
+        void UploadFileAsync(string filePath, string destinationPath);
+        void CreateDirectoryAsyns(string directoryPath);
+        List<string> GetFilesAndDirectoryAsync(string directoryPath);
+        Stream DownloadFileAsync(string filePath);
+        Stream DownloadFolderAsZipAsync(string directoryPath);
+        void DeleteFileAsyns(string filePath);
+        void DeleteDirectoryAsyns(string filePath);
+    }
+}
