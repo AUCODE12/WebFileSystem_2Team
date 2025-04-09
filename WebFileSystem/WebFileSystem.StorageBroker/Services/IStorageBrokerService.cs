2e8fb7698dbdb6c2de3f52aae6b80535c39ddb2c
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-04-09 14:10:18 +0500

    Everything is ready, it just needs to be implement.

---
+++ WebFileSystem/WebFileSystem.StorageBroker/Services/IStorageBrokerService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebFileSystem.StorageBroker.Services;
+
+public interface IStorageBrokerService
+{
+    Task UploadFileAsync(string filePath, Stream stream);
+    Task CreateDirectoryAsync(string directoryPath);
+    Task<List<string>> GetAllFilesAndDirectoriesAsync(string directoryPath);
+    Task<Stream> DownloadFileAsync(string filePath);
+    Task<Stream> DownloadFolderAsZipAsync(string directoryPath);
+    Task DeleteFileAsync(string filePath);
+    Task DeleteDirectoryAsync(string directoryPath);
+}
